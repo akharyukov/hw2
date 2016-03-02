1e79f372db9ac6221b705fdb54df4af6ffdbf54c
akharyukov <kharyukov@gmail.com>
2016-03-03 00:43:02 +0300

    Init

---
+++ src/com/example/helloworld/HelloWorld.java
@@ -0,0 +1,11 @@
+package com.example.helloworld;
+
+/**
+ * Created by Khas on 02.03.2016.
+ */
+public class HelloWorld {
+    public static void main(String[] args){
+        System.out.println("Hellow, World!");
+    }
+}
+
